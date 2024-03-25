acd75a8674bb4bd04082c8838bfd37c8215b2e7d
François de Kermenguy <francois.de-kermenguy@outlook.fr>
2024-03-25 18:23:41 +0100

    final

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 import sv
-import vtk
 import csv
 import os
-import argparse
-import logging
-import sys
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom.minidom import parseString
 
@@ -14,7 +10,8 @@ from xml.dom.minidom import parseString
 # This script is used to calculate the
 # angle between the planes of two
 # contours. (contour_id1, contour_id2)
-# To lauch this code: Start-Process -FilePath "C:\Program Files\SimVascular\SimVascular\2023-03-27\sv.bat"  -NoNewWindow -ArgumentList "--python", "-- C:\Users\franc\Documents\Code_python\dcm2ctgr\angle_between_contours.py"
+# To lauch this code: 
+# Start-Process -FilePath "C:\Program Files\SimVascular\SimVascular\2023-03-27\sv.bat"  -NoNewWindow -ArgumentList "--python", "-- C:\Users\franc\Documents\Code_python\dcm2ctgr\main.py"
 # ######################################
 # https://github.com/SimVascular/SimVascular-Tests/blob/master/new-api-tests/pathplanning/create.py
 # use help(sv.pathplanning) to get the list of functions
@@ -49,7 +46,7 @@ def _get_control_points_id(path):
     return cp_id_corres_dict
 
 
-def generate_contour(cp, id, time_step, path, curve_id):
+def generate_contour(cp, id, time_step, path, curve_id, scaling_factor = 1.0):
 
     # path.get_curve_point(curve_id), path.get_control_point(id) and seg.get_center() should be the same point
     if path.get_curve_point(curve_id) != path.get_control_points()[id]:
@@ -70,9 +67,9 @@ def generate_contour(cp, id, time_step, path, curve_id):
     path_point.set('id', str(curve_id))
     pos_data = path.get_curve_tangent(curve_id)
     pos = SubElement(path_point, 'pos')
-    pos.set('x', str(pos_data[0]))
-    pos.set('y', str(pos_data[1]))
-    pos.set('z', str(pos_data[2]))
+    pos.set('x', str(pos_data[0] * scaling_factor))
+    pos.set('y', str(pos_data[1] * scaling_factor))
+    pos.set('z', str(pos_data[2] * scaling_factor))
 
     tangent = SubElement(path_point, 'tangent')
     tangent_data = path.get_curve_tangent(curve_id)
@@ -89,11 +86,12 @@ def generate_contour(cp, id, time_step, path, curve_id):
     control_points = SubElement(contour, 'control_points')
     contour_points = SubElement(contour, 'contour_points')
     for k, point in enumerate(cp):
-        SubElement(control_points, 'point', id=str(k), x=str(point[0]), y=str(point[1]), z=str(point[2]))
-        SubElement(contour_points, 'point', id=str(k), x=str(point[0]), y=str(point[1]), z=str(point[2]))
+        x_pos, y_pos, z_pos = str(point[0] * scaling_factor), str(point[1] * scaling_factor), str(point[2] * scaling_factor)
+        SubElement(control_points, 'point', id=str(k), x=x_pos, y=y_pos, z=z_pos)
+        SubElement(contour_points, 'point', id=str(k), x=x_pos, y=y_pos, z=z_pos)
 
 
-def rtss2pth(contour_data, roi_name, output_path, scaling_factor = 1.0):
+def rtss2pth(contour_data, roi_name, roi_id, output_path, scaling_factor = 1.0):
     ''' Create a segmentation from a path curve point.
     https://github.com/SimVascular/SimVascular-Tests/blob/master/new-api-tests/segmentation/polygon-path.py
     '''
@@ -104,19 +102,20 @@ def rtss2pth(contour_data, roi_name, output_path, scaling_factor = 1.0):
     path_control_points = []
     for _, control_points in contour_data[roi_name].items():
         seg = sv.segmentation.Contour(control_points)
-        path_control_points.append(seg.get_center())
+        path_control_points.append([x * scaling_factor for x in seg.get_center()])
 
     path.set_control_points(path_control_points)
 
     path_serie = sv.pathplanning.Series()
-    path_serie.set_name(roi_name)
+    path_serie.set_name("{}_path.pth".format(roi_name))
     path_serie.set_path(path, 0)
-    path_serie.set_path_id(1)
+    path_serie.set_path_id(roi_id)
     path_serie.write(os.path.join(output_path["pth"], "{}_path.pth".format(roi_name)))
 
-    # Le code suivant ne fonctionne pas. Le Pyhton API de SimVascular est encore en beta et lessv.segmentation.Series() 
-    #n'a pas toutes les fonctions implémentées comme sv.pathplanning.Series().
-    # Pour voir la documentation, utiliser import sv puis help(sv.segmentation.Series())
+    # Le code suivant ne fonctionne pas. Le Pyhton API de SimVascular est encore en beta et sv.segmentation.Series() 
+    #n'a pas encore toutes les fonctions de sv.pathplanning.Series().
+    # Pour voir la documentation, utiliser import sv puis help(sv.segmentation.Series()) dans le shell de SimVascular
+    # Pour lancer le shell de SimVascular, utilisez Start-Process -WorkingDirectory "C:\Program Files\SimVascular\SimVascular\2023-03-27\" -FilePath "sv.bat" --python                                                                                                           
     # cf https://simtk.org/plugins/phpBB/viewtopicPhpbb.php?f=188&t=17705&p=0&start=0&view=&sid=c6f5ddfd0afc0410cf9084c2dc473622
     """     
     contour_serie = sv.segmentation.Series()
@@ -130,11 +129,11 @@ def rtss2pth(contour_data, roi_name, output_path, scaling_factor = 1.0):
 
 def rtss2ctgr(contour_data, roi_name, roi_id, output_path, scaling_factor = 1.0):
 
-    path = rtss2pth(contour_data, roi_name, output_path)
+    path = rtss2pth(contour_data, roi_name, roi_id, output_path, scaling_factor)
 
     # Create a segmentation.
     contour_group = Element("contourgroup")
-    contour_group.set("path_name", roi_name)
+    contour_group.set("path_name", "{}_path.pth".format(roi_name))
     contour_group.set("path_id", str(roi_id))
     contour_group.set("reslice_size", "5")
     contour_group.set("point_2D_display_size", "")
@@ -149,18 +148,18 @@ def rtss2ctgr(contour_data, roi_name, roi_id, output_path, scaling_factor = 1.0)
     cp_id_corres_dict = _get_control_points_id(path)
     for id, cp in contour_data[roi_name].items():
         curve_id = cp_id_corres_dict[id]
-        generate_contour(cp, id, time_step, path, curve_id)
+        generate_contour(cp, id, time_step, path, curve_id, scaling_factor = scaling_factor)
         
     #Save as .ctgr file
     xml_ctgr = parseString(tostring(contour_group, encoding='UTF-8')).toprettyxml(indent="   ")
-    with open(os.path.join(output_path["ctgr"], roi_name + ".ctgr"), 'w') as f:
+    with open(os.path.join(output_path["ctgr"], roi_name + "_segmentation.ctgr"), 'w') as f:
         f.write(xml_ctgr)
 
 
 absolute_path = os.path.dirname(__file__)
-contour_data_path = os.path.join(absolute_path, "contour_data.csv")
+contour_data_path = os.path.join(absolute_path, "contour_data_2.csv")
 output_path = {"pth": os.path.join(absolute_path,"SV_test//Paths"), "ctgr" :  os.path.join(absolute_path, "SV_test\Segmentations")}
 contour_data = load_csv_contour_data(contour_data_path)
 
 for roi_id, roi_name in enumerate(contour_data.keys()):
-    rtss2ctgr(contour_data, roi_name, roi_id, output_path)
+    rtss2ctgr(contour_data, roi_name, roi_id, output_path, scaling_factor=0.1)
--- parse_rtss.py
+++ parse_rtss.py
@@ -1,23 +1,24 @@
 import pydicom
 import numpy as np
 import csv
+import os
 
-def parse_ROINumber(ds, roi_name):
+def _parse_ROINumber(ds, roi_name):
     for roi in ds.StructureSetROISequence:
         if roi.ROIName == roi_name:
             return roi.ROINumber
     return None
 
-def check_if_rtss(dicom_file):
+def _check_if_rtss(dicom_file):
     if not dicom_file.Modality == 'RTSTRUCT':
         raise ValueError('The input file is not a RTSTRUCT file.')
 
-def parse_rtss(rtss, roi_name, output_path):
-    check_if_rtss(rtss)
+def parse_rtss(rtss, roi_name):
+    _check_if_rtss(rtss)
 
     control_point_contour = {}
 
-    roi_number = parse_ROINumber(rtss, roi_name)
+    roi_number = _parse_ROINumber(rtss, roi_name)
     for roi in rtss.ROIContourSequence:
             if roi.ReferencedROINumber == roi_number:
                     for contour_sequence in roi.ContourSequence:
@@ -29,34 +30,20 @@ def parse_rtss(rtss, roi_name, output_path):
     return control_point_contour
 
 
-
-rtss_path = "C://Users//franc//Documents//Code_python//dcm2ctgr//data//AGORL_P66//1.3.12.2.1107.5.1.4.49226.30000019112706455609300000818--1.2.276.0.7230010.3.1.4.296486144.178182.1685377991.264831_rtss.dcm"  
-output_path = "C://Users//franc//Documents//Code_python//dcm2ctgr//output"
-rtss = pydicom.dcmread(rtss_path)
+absolute_path = os.path.dirname(__file__)
+rtss_path = absolute_path + "//data//AGORL_P66//RTSTRUCT//1.3.12.2.1107.5.1.4.49226.30000019112706455609300000818--1.2.276.0.7230010.3.1.4.296486144.178182.1685377991.264831_rtss.dcm"
+rtss_path_2 = absolute_path + "//data//AGORL_P66//RTSTRUCT//1.3.12.2.1107.5.1.4.49226.30000019112706455609300000818--1.2.276.0.7230010.3.1.4.548144204.2639.1685347439.482282_rtss.dcm"
+output_path = absolute_path
+rtss = pydicom.dcmread(rtss_path_2)
 roi_names = [roi.ROIName for roi in rtss.StructureSetROISequence]
 
 contour_data = {}
 for roi_name in roi_names:
-    control_point_contour = parse_rtss(rtss, roi_name, output_path)
+    control_point_contour = parse_rtss(rtss, roi_name)
     contour_data[roi_name] = control_point_contour
 
-
-with open('contour_data.csv', 'w') as f:
+with open('contour_data_2.csv', 'w') as f:
     w = csv.DictWriter(f, contour_data.keys())
     w.writeheader()
     w.writerow(contour_data)
-    
-def load_contour_data(csv_file_path):
-    loaded_data = {}
-    with open(csv_file_path, 'r', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            for roi, data in row.items():
-                loaded_data[roi] = eval(data)
-    return loaded_data
-                        
-
-contour_data = load_contour_data("contour_data.csv")
-for control_point in contour_data[roi_name].items():
-    print(control_point)
 
